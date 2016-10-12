460287196115c9aa2a8274722ca991541f71723c
Francesco Bausi <fbaus@users.noreply.github.com>
2016-10-12 08:21:04 +0100

    Update likeliness.py

--- likeliness.py
+++ likeliness.py
@@ -5,7 +5,9 @@ import scipy.misc as sm
 import numpy as np
 import tkinter
 
-def thumbn(c)    #CREATES SQUARE THUMBNAIL COPIES (size c-by-c) of the IMAGES
+def main():#<<<CREATE STRUCTURE of the PROGRAM
+
+def thumbn(c)                                           #CREATES SQUARE THUMBNAIL COPIES (size c-by-c) of the IMAGES
 l = {}
 for filename in os.listdir( os.getcwd() ):              #for all files into folder
     a = sm.imread( filename )                           ###import image as array <<DOES IT WORK THE SAME FOR DIFFERENT FORMATS?
@@ -16,6 +18,7 @@ for filename in os.listdir( os.getcwd() ):              #for all files into fold
     l[ name ] = 'n'                                     #add the name to the blacklist but as non-duplicate
     
 
+def compare() #<<<
 d=[0]                                                   #list of duplicated groups
 #IT WILL BE A LIST OF LISTS [[.,.],[.,.,.],[.,.]] WHERE EACH SUBLIST CONTAINS GROUPS OF DUPLICATES
 b=0                                                     #counter for how many i have duplicates i.e. how many groups in the list
@@ -24,8 +27,8 @@ for i in l.keys():                                      #loop on every file in t
         c=0                                             #counter for how many duplicates for i i.e. how many pics in one group
         for j in l.keys():                              #loop on every file in the folder
             if i!=j & l[j]=='n':                        #if i and j are different
-                #compare i and j DEVELOP METHOD TO COMPARE THUMBNAILS
-                if #compare = match above threshold:                    
+                #<<<compare i and j DEVELOP METHOD TO COMPARE THUMBNAILS
+                if #<<<compare = match above threshold:                    
                     l[ j ] = 'y'                        #mark picture j as duplicate
                     if c=0                              #if j is the first duplicate of i
                         l[ i ] = 'y'                    #mark picture i as duplicate
@@ -36,8 +39,8 @@ for i in l.keys():                                      #loop on every file in t
                     b=b+1                               #increment the number of i that have duplicates (i.e. number of groups)
 
 
-#ADD TKINTER to DISPLAY ELEMENT of the GROUPS of d LIST of LISTS to TICK the REAL DUPLICATES. 
-#THE ORIGINAL IS USUALLY THE ONE WITH BEST RESOLUTION
+#<<<ADD TKINTER to DISPLAY ELEMENT of the GROUPS of d LIST of LISTS to TICK the REAL DUPLICATES. 
+#<<<THE ORIGINAL IS USUALLY THE ONE WITH BEST RESOLUTION
 
 
 
@@ -68,5 +71,6 @@ else: M=False
     
 
     
-if __name__ == '__main__':
+if __name__ == '__main__': #a call to main() at the bottom so that each def is executed by the time we call main()
+#Script's variable __name__ has the value '__main__' only when the script is run, not imported.
     main()
